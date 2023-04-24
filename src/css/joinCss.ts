61ba51f1def553e81d9e62f742ada0db21fb8b3e
Matthew 𝔸𝕝𝕕𝕒𝕔𝕠 <Matt.Aldaco@csiweb.com>
2023-04-24 15:19:35 -0500

    Added documentation; fixed parameter bug

--- src/css/joinCss.ts
+++ src/css/joinCss.ts
@@ -1,4 +1,22 @@
+/**
+ * Joins together multiple strings into a single CSS class string.
+ *
+ * Good for separating conditional logic for classes into multiple lines more
+ * easily.
+ *
+ * @example
+ *
+ * const css: string = joinCss(
+ *     'w-full h-full p-8 transition duration-150',
+ *     darkMode ? 'border-white text-white' : 'border-black text-black',
+ *     showBorder ? 'border' : ''
+ * );
+ *
+ * @param classes An array of strings which join into a single CSS class.
+ *
+ * @returns A single string of combined CSS classes.
+ */
 export const joinCss = (
-    classes: (string | null | undefined)[]
+    ...classes: (string | null | undefined)[]
 ) =>
     classes.filter(Boolean).join(' ');
